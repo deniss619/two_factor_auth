9e00f0ff64f3e186a61d5e53425c9c0bf16639f8
Deniss <619deniss61999@gmail.com>
2020-03-29 18:21:25 +0300

    изменен тип данных в базе данных для counter

--- sweater/models.py
+++ sweater/models.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
     login = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     pass_img = db.Column(db.String(255), nullable=False)
-    counter = db.Column(db.String(16), default=0)
+    counter = db.Column(db.Integer, default=0)
 
 
 class Image(db.Model, UserMixin):
